86dac041a205a7bdde604be61ddac5facc5a9a92
Yavuz <yvzerv@gmail.com>
2025-02-17 09:53:51 +0300

    Başlangıç

---
+++ src/test/java/org/deneme/denemebackend/DenemeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.deneme.denemebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DenemeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
